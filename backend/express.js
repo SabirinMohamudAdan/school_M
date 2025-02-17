0204c52cea809b66fe989923716b8f8ad89fdecf
SabirinMohamudAdan <sabrinadan327@gmail.com>
2025-02-16 23:24:50 -0800

    up

--- backend/express.js
+++ backend/express.js
@@ -1,8 +1,10 @@
 const express =require("express");
 const mongoose = require("mongoose");
 const schoolSchema = require("./model/schoolSchema");
+const cors=require("cors")
 
 const app=express()
+app.use(cors())
 
 
 app.use(express.json())
